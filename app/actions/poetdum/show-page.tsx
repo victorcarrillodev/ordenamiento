619283f799819fcc5cf0bdebe97587ebc55b346a
manuel <manuelgonzalez211104@gmail.com>
2026-08-22 12:56:21 -0600

    se crea componete nav para hacerlo reutilizable

--- app/actions/poetdum/show-page.tsx
+++ app/actions/poetdum/show-page.tsx
@@ -1,10 +1,13 @@
 import type { Handle } from "remix/ui";
 import { Document } from "../document.tsx";
 
+import {NavBar} from "../../components/Nav.tsx"
 export function PoetdumPage(handle: Handle<{ id: string }>) {
   return () => {
     return (
       <Document title="poetdum">
+    <NavBar/>
+
         <main>
           <h1>Hola mundo desde poetdum</h1>
         </main>
